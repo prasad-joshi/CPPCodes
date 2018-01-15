b6c55fd9044a24d2c36646189747468b6a266839
Prasad Joshi <prasadjoshi.linux@gmail.com>
2018-01-16 01:53:55 +0530

    Working frontend
    Signed-off-by: Prasad Joshi <prasadjoshi.linux@gmail.com>

--- Hex/src/HexGame.cpp
+++ Hex/src/HexGame.cpp
@@ -5,5 +5,58 @@
 
 using namespace Game;
 int main() {
+	auto quit = false;
+	while (not quit) {
+		HexBoard board(7);
+		Player player = Player::kPlayer1;
+		while (not board.IsGameOver(player) and not quit) {
+			std::cout << board << std::endl;
+			std::cout << "Current player " << PlayerToString(player) << std::endl;
+			if (player == Player::kPlayer1) {
+				std::cout << "Chain " << PlayerToString(player)
+					<< " from top to bottom " << std::endl;
+			} else {
+				std::cout << "Chain " << PlayerToString(player)
+					<< " from left to right " << std::endl;
+			}
+
+			int16_t row, col;
+			bool first = true;
+			do {
+				if (not first) {
+					std::cout << "(" << row << "," << col << ") is not free"
+						<< std::endl;
+				}
+				std::cout << "Enter row and col [(-1 -1) to quit] : ";
+				std::cin >> row >> col;
+				first = false;
+				if (row == -1) {
+					quit = true;
+				}
+			} while (not board.IsFree(Hex(col, row)) and not quit);
+
+			if (not quit) {
+				board.PlayerPlayed(Hex(col, row), player);
+				player = NextPlayer(player);
+			}
+		}
+
+		if (not quit) {
+			if (not board.HasWinner()) {
+				std::cout << "Game drawn" << std::endl;
+			} else {
+				std::cout << PlayerToString(board.GetWinner())
+					<< " Won!!!" << std::endl;
+			}
+
+			std::string input;
+			std::cout << "Quit [y/n]? ";
+			std::cin >> input;
+
+			if (input.front() == 'y') {
+				quit = true;
+			}
+		}
+	}
 	return 0;
 }
