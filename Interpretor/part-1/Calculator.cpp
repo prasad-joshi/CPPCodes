c2c7c4be153681d88775ff8efb3ff57961593f88
Prasad Joshi <prasadjoshi.linux@gmail.com>
2019-01-15 22:45:08 +0530

    Simple Interpreted calculator
    Signed-off-by: Prasad Joshi <prasadjoshi.linux@gmail.com>

---
+++ Interpretor/part-1/Calculator.cpp
@@ -0,0 +1,194 @@
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+#include <cctype>
+#include <cmath>
+#include <cassert>
+
+enum class TokenType {
+	kEof,
+	kInteger,
+	kPlus,
+	kMinus,
+	kMultiply,
+	kDivide,
+	kMod,
+	kPower,
+};
+
+std::ostream& operator << (std::ostream& os, TokenType rhs) {
+	switch (rhs) {
+	case TokenType::kEof:
+		os << "eof";
+		break;
+	case TokenType::kInteger:
+		os << "integer";
+		break;
+	case TokenType::kPlus:
+		os << "plus";
+		break;
+	case TokenType::kMinus:
+		os << "minus";
+		break;
+	case TokenType::kMultiply:
+		os << "mul";
+		break;
+	case TokenType::kDivide:
+		os << "div";
+		break;
+	case TokenType::kMod:
+		os << "mod";
+		break;
+	case TokenType::kPower:
+		os << "power";
+		break;
+	}
+	return os;
+}
+
+class Token {
+public:
+	Token(TokenType type, std::string&& value) :
+		value_(std::forward<std::string>(value)), type_(type) {
+	}
+
+	Token(TokenType type) : type_(type) {
+	}
+
+	Token(TokenType type, char value) : value_(1, value), type_(type) {
+	}
+
+	const std::string& Value() const noexcept {
+		return value_;
+	}
+
+	TokenType GetTokenType() const noexcept {
+		return type_;
+	}
+
+	friend std::ostream& operator << (std::ostream& os, const Token& rhs) {
+		return os << "Token({" << rhs.type_ << "}, {" << rhs.value_ << "})";
+	}
+private:
+	std::string value_;
+	TokenType type_;
+};
+
+class Interpretor {
+public:
+	Interpretor(std::string&& text) : text_(std::forward<std::string>(text)),
+			it_(text_.begin()), end_(text_.end()) {
+	}
+
+	int32_t Expr() {
+		current_.token_ = NextToken();
+
+		auto left = current_.token_;
+		assert(left.GetTokenType() == TokenType::kInteger);
+		Eat(left.GetTokenType());
+
+		auto op = current_.token_;
+		Eat(op.GetTokenType());
+
+		auto right = current_.token_;
+		assert(right.GetTokenType() == TokenType::kInteger);
+
+		switch (op.GetTokenType()) {
+		default:
+			assert(0);
+			return -EINVAL;
+		case TokenType::kPlus:
+			return std::stoi(left.Value()) + std::stoi(right.Value());
+		case TokenType::kMinus:
+			return std::stoi(left.Value()) - std::stoi(right.Value());
+		case TokenType::kMultiply:
+			return std::stoi(left.Value()) * std::stoi(right.Value());
+		case TokenType::kDivide:
+			return std::stoi(left.Value()) / std::stoi(right.Value());
+		case TokenType::kMod:
+			return std::stoi(left.Value()) % std::stoi(right.Value());
+		case TokenType::kPower:
+			return std::pow(std::stoi(left.Value()), std::stoi(right.Value()));
+		}
+	}
+
+private:
+	void Error() {
+		std::string error("Unknown token: ");
+		error += *it_;
+		throw std::runtime_error(std::move(error));
+	}
+
+	Token NextToken() {
+		while (it_ != end_ and std::isspace(*it_)) {
+			++it_;
+		}
+		if (it_ == end_) {
+			return Token(TokenType::kEof);
+		}
+
+		if (std::isdigit(*it_)) {
+			auto begin = it_;
+			while (it_ != end_ and std::isdigit(*it_)) {
+				++it_;
+			}
+			std::string v(begin, it_);
+			return Token(TokenType::kInteger, std::move(v));
+		}
+
+		switch (*it_) {
+		case '+':
+			++it_;
+			return Token(TokenType::kPlus);
+		case '-':
+			++it_;
+			return Token(TokenType::kMinus);
+		case '*':
+			++it_;
+			return Token(TokenType::kMultiply);
+		case '/':
+			++it_;
+			return Token(TokenType::kDivide);
+		case '%':
+			++it_;
+			return Token(TokenType::kMod);
+		case '^':
+			++it_;
+			return Token(TokenType::kPower);
+		defult:
+			Error();
+		}
+		return current_.token_;
+	}
+
+	void Eat(TokenType type) {
+		if (type != current_.token_.GetTokenType()) {
+			Error();
+		}
+		current_.token_ = NextToken();
+	}
+private:
+	std::string text_;
+	std::string::iterator it_{};
+	std::string::iterator end_{};
+
+	struct {
+		Token token_{TokenType::kEof};
+	} current_;
+};
+
+int main() {
+	while (true) {
+		std::string input;
+		std::cout << "calc> ";
+		std::getline(std::cin, input);
+		if (input.empty()) {
+			std::cout << "DONE" << std::endl;
+			break;
+		}
+
+		Interpretor intpretor(std::move(input));
+		std::cout << intpretor.Expr() << std::endl;
+	}
+}
